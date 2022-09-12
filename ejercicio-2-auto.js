7c4e2370795cebfd0a479b319188706e71c8e7f0
Emiliano Gonzalez <emilianogonzalez@tdf.edu.ar>
2022-09-11 21:02:59 -0300

    se agrego export

--- ejercicio-2-auto.js
+++ ejercicio-2-auto.js
@@ -1,3 +1,5 @@
+"use strict";
+exports.__esModule = true;
 var auto = /** @class */ (function () {
     /* constructor */
     function auto(paramMarca, paramModelo, paramAnio, paramColor, paramChasis, paramEsNuevo) {
@@ -31,19 +33,24 @@ var auto = /** @class */ (function () {
     };
     ;
     auto.prototype.pedirEsNuevo = function () {
-        this.esnuevo.toString();
         return this.esnuevo;
     };
     ;
     return auto;
 }());
 ;
+var registroAutmotor = /** @class */ (function () {
+    function registroAutmotor() {
+    }
+    return registroAutmotor;
+}());
+;
 /* creacion de hijos */
 var auto1 = new auto("vw", "gol", 2012, "azul como el mar azul", 123456, false);
-var auto2 = new auto("fiat", "argo", 2022, "rojo pasion", 12346, true);
-var auto3 = new auto("chevrolet", "corsa", 2008, "amarillo patito", 12347, false);
-var auto4 = new auto("renault", "logan ii", 2020, "verde manzana", 12347, true);
-var auto5 = new auto("ford", "fiesta kinetic", 2015, "negro como la noche", 12348, false);
+var auto2 = new auto("fiat", "argo", 2022, "rojo pasion", 123457, true);
+var auto3 = new auto("chevrolet", "corsa", 2008, "amarillo patito", 123458, false);
+var auto4 = new auto("renault", "logan ii", 2020, "verde manzana", 123459, true);
+var auto5 = new auto("ford", "fiesta kinetic", 2015, "negro como la noche", 123460, false);
 /* console logs */
 console.log("----------------------");
 console.log("los vehiculos cargados son:");
--- ejercicio-2-auto.ts
+++ ejercicio-2-auto.ts
@@ -1,3 +1,4 @@
+export{}
 class auto {
     /* atributos */
     private marca : string;
@@ -39,15 +40,22 @@ class auto {
         return this.esnuevo;
     };
 
+
+};
+
+class registroAutmotor{
+    
 };
 
 /* creacion de hijos */
 
 let auto1 = new auto("vw","gol",2012,"azul como el mar azul",123456,false);
-let auto2 = new auto("fiat","argo",2022,"rojo pasion",12346,true);
-let auto3 = new auto("chevrolet","corsa",2008,"amarillo patito",12347,false);
-let auto4 = new auto("renault","logan ii",2020,"verde manzana",12347,true);
-let auto5 = new auto("ford","fiesta kinetic", 2015,"negro como la noche",12348,false);
+let auto2 = new auto("fiat","argo",2022,"rojo pasion",123457,true);
+let auto3 = new auto("chevrolet","corsa",2008,"amarillo patito",123458,false);
+let auto4 = new auto("renault","logan ii",2020,"verde manzana",123459,true);
+let auto5 = new auto("ford","fiesta kinetic", 2015,"negro como la noche",123460,false);
+
+
 
 /* console logs */
 
@@ -57,4 +65,4 @@ console.log(auto1, auto2, auto3, auto4, auto5);
 
 /* funciones */
    
-    
+   
